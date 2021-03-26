22d60d1f16ccbad693cbd3838ff2aec7e44c455a
tomppk <tomi.kivioja@gmail.com>
2021-03-26 15:46:49 +0200

    add get and set to User

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('hello world');
+import { User } from './models/User';
+
+const user = new User({ name: 'myname', age: 20 });
+
+console.log(user.get('name'));
+console.log(user.get('age'));
+
+user.set({ name: 'johnny', age: 30 });
+console.log(user.get('name'));
+console.log(user.get('age'));
+
+user.set({ name: 'mickey' });
+
+console.log(user.get('name'));
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,22 @@
 // Extract UserProps object from constructor. Better to have separate interface
-// than defining the object in one line on constructor
+// than defining the object in one line on constructor.
+// Question mark ? after property name makes a property optional, not required
 interface UserProps {
-  name: string;
-  age: number;
+  name?: string;
+  age?: number;
 }
 
 export class User {
   constructor(private data: UserProps) {}
+
+  // Get User properties name or age
+  get(propName: string): number | string {
+    return this.data[propName];
+  }
+
+  // Set UserProps with Object.assign(target, copyFrom). Copy object
+  // properties from object to target object. Properties are overwritten
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
 }
