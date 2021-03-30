0460dee407c828caf0b3dab5f4cc685a5cb5cb2c
tomppk <tomi.kivioja@gmail.com>
2021-03-30 10:46:11 +0300

    extract functionality from User to Sync class

---
+++ src/models/Sync.ts
@@ -0,0 +1,32 @@
+import axios, { AxiosPromise } from 'axios';
+import { UserProps } from './User';
+
+export class Sync {
+  // http://localhost:3000/users
+  constructor(public rootUrl: string) {}
+
+  // Make network get request to backend to fetch user data using id of
+  // current User instance. Update all properties of current user instance
+  // Return Axios promise that resolves into fetched data object we can then
+  // use to update our User class properties
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootUrl}/${id}`);
+  }
+
+  // If there already exists a user with this id then update that user with put
+  // otherwise post a new user to backend database
+  // Return AxiosPromise (same as regular Promise but used with axios) so we get
+  // back an object we can use to determine whether the save to backend was
+  // successful
+  save(data: UserProps): AxiosPromise {
+    const { id } = data;
+
+    if (id) {
+      // put (url, new data)
+      return axios.put(`${this.rootUrl}/${id}`, data);
+    } else {
+      // post (url, new data)
+      return axios.post(this.rootUrl, data);
+    }
+  }
+}
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,10 +1,9 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
 
 // Extract UserProps object from constructor. Better to have separate interface
 // than defining the object in one line on constructor.
 // Question mark ? after property name makes a property optional, not required
-interface UserProps {
+export interface UserProps {
   id?: number;
   name?: string;
   age?: number;
@@ -24,29 +23,5 @@ export class User {
   // properties from object to target object. Properties are overwritten
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  // Make network get request to backend to fetch user data using id of
-  // current User instance. Update all properties of current user instance
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get('id')}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-
-  // If there already exists a user with this id then update that user with put
-  // otherwise post a new user to backend database
-  save(): void {
-    const id = this.get('id');
-
-    if (id) {
-      // put (url, new data)
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      // post (url, new data)
-      axios.post('http://localhost:3000/users', this.data);
-    }
   }
 }
