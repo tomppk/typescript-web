f80cb0ebaa519d90fe09a7b92ca25a92a8235ad1
tomppk <tomi.kivioja@gmail.com>
2021-04-01 10:23:40 +0300

    refactor UserForm into abstract View parent class

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,24 +1,6 @@
-import { User } from '../models/User';
+import { View } from './View';
 
-// Element is a reference to any HTML element
-// When creating new instance of UserForm we pass in
-// reference to parent element where we want to render
-// the html form.
-// Take in model of User class
-export class UserForm {
-  constructor(public parent: Element, public model: User) {
-    this.bindModel();
-  }
-
-  // Adds event listener to User model that listens for
-  // 'change' event and calls this.render() when there is
-  // change in User model
-  bindModel(): void {
-    this.model.on('change', () => {
-      this.render();
-    });
-  }
-
+export class UserForm extends View {
   // Connects the event we want to watch for and function to run for that event.
   // event we want to listen for and element we are adding the event listener.
   // Returns an object with a key of type string and value of type function that
@@ -65,47 +47,5 @@ export class UserForm {
             <button class="set-age">Set Random Age</button>
         </div>
         `;
-  }
-
-  // Takes in a fragment of HTML which is defined in template()
-  bindEvents(fragment: DocumentFragment): void {
-    const eventsMap = this.eventsMap();
-
-    // For each key: value pair in eventsMap object
-    // create two new variables eventName and selector by splitting
-    // eventKey at :
-    // Split returns an array of split elements
-    // eventName is 'click' and selector 'button'
-    for (let eventKey in eventsMap) {
-      const [eventName, selector] = eventKey.split(':');
-
-      // Select all HTML fragments that match selector eg. 'button'
-      // For each such element addEventlistener for eventName eg. 'click'
-      // and the callback function to run in case of event is found at the
-      // value of eventsMap at key eventKey eg. 'click:button'
-      fragment.querySelectorAll(selector).forEach((element) => {
-        element.addEventListener(eventName, eventsMap[eventKey]);
-      });
-    }
-  }
-
-  // <template> elements can be used to turn a string into
-  // HTML element
-  // append the HTML content of template element into parent element
-  // .content property is a reference to the actual HTML inside template element
-  // .content is a type of DocumentFragment. This is an object that holds the
-  // HTML in memory before it is inserted into the DOM
-  // bindEvents() looks through templateElement HTML content if there is matching
-  // element name in there and eventsMap. Then it adds event listener to that
-  // element and a callback to run as defined in eventsMap
-  // Before each re-render empty the parent elements html content.
-  render(): void {
-    this.parent.innerHTML = '';
-    const templateElement = document.createElement('template');
-    templateElement.innerHTML = this.template();
-
-    this.bindEvents(templateElement.content);
-
-    this.parent.append(templateElement.content);
   }
 }
