014e771d0d1d9f150bb8f546959c3358287504f4
tomppk <tomi.kivioja@gmail.com>
2021-04-01 13:18:13 +0300

    refactor UserForm to only contain form elements

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -10,8 +10,14 @@ export class UserForm extends View<User, UserProps> {
     return {
       'click:.set-age': this.onSetAgeClick,
       'click:.set-name': this.onSetNameClick,
+      'click:.save-model': this.onSaveClick,
     };
   }
+
+  // Save user into database
+  onSaveClick = (): void => {
+    this.model.save();
+  };
 
   // Select <input> tag from parent DOM element.
   // Access input property value which is the text inside input field
@@ -40,12 +46,10 @@ export class UserForm extends View<User, UserProps> {
   template(): string {
     return `
         <div>
-            <h1>User Form</h1>
-            <div>User name: ${this.model.get('name')}</div>
-            <div>User age: ${this.model.get('age')}</div>
-            <input />
+            <input placeholder="${this.model.get('name')}" />
             <button class="set-name">Change Name</button>
             <button class="set-age">Set Random Age</button>
+            <button class="save-model">Save User</button>
         </div>
         `;
   }
